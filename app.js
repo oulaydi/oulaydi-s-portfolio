4bba79770d6915965c7f164a8363d81b30e07967
oussama oulaydi <130802700+oulaydi@users.noreply.github.com>
2023-04-30 20:57:31 +0100

    added images, Js and CV

---
+++ app.js
@@ -0,0 +1,25 @@
+ScrollReveal({
+    duration: 1500,
+    distance: "60px",
+});
+
+
+ScrollReveal().reveal('.programming', { delay: 10});
+ScrollReveal().reveal('.sub-heading', { delay: 1000});
+ScrollReveal().reveal('.heading', { delay: 100});
+ScrollReveal().reveal('.text', { delay: 1800});
+
+/* btn */
+
+ScrollReveal().reveal(".cta .second-btn", { delay: 2500});
+ScrollReveal().reveal(".cta .btn", { delay: 2000});
+ScrollReveal().reveal(".btn-hire", { delay: 2000});
+ 
+/* work */
+ScrollReveal().reveal('.text-work', { delay: 300});
+ScrollReveal().reveal('.heading-work', { delay: 100});
+ScrollReveal().reveal('.heading-service', { delay: 100});
+
+/* pics */
+
+ScrollReveal().reveal('.image-container', { delay: 1800});
